24a65e2c9ee5324c66c9bef933b93d9cddfe0a7b
Star7-jpg <marcosconde612@gmail.com>
2024-02-20 12:41:35 -0600

    08  Finishing first version of AppThemes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,7 +8,9 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('App de componentes de flutter'),
+        title: const Text(
+          'App de componentes de flutter',
+        ),
       ),
       body: ListView(
         children: <Widget>[
@@ -20,36 +22,54 @@ class HomeScreen extends StatelessWidget {
             ),
             subtitle: Text(
               'Recuperar información de TextField',
-              style: Theme.of(context).textTheme.bodySmall,
+              style: AppTheme.ligthTheme.textTheme.bodySmall,
             ),
-            leading: const Icon(Icons.input_rounded),
-            trailing: const Icon(Icons.arrow_right_alt_outlined),
+            leading: const Icon(
+              Icons.input_rounded,
+              color: AppTheme.primaryColor,
+            ),
+            trailing: const Icon(
+              Icons.arrow_right_alt_outlined,
+              color: AppTheme.primaryColor,
+            ),
           ),
           const Divider(),
           ListTile(
             title: Text(
               'Lista infinita',
-              style: Theme.of(context).textTheme.headlineLarge,
+              style: AppTheme.ligthTheme.textTheme.headlineLarge,
             ),
             subtitle: Text(
               'Recuperar muchos elementos',
-              style: Theme.of(context).textTheme.bodySmall,
+              style: AppTheme.ligthTheme.textTheme.bodySmall,
             ),
-            leading: const Icon(Icons.list_alt_sharp),
-            trailing: const Icon(Icons.arrow_right_alt_outlined),
+            leading: const Icon(
+              Icons.list_alt_sharp,
+              color: AppTheme.primaryColor,
+            ),
+            trailing: const Icon(
+              Icons.arrow_right_alt_outlined,
+              color: AppTheme.primaryColor,
+            ),
           ),
           const Divider(),
           ListTile(
             title: Text(
               'Notificaciones',
-              style: Theme.of(context).textTheme.headlineLarge,
+              style: AppTheme.ligthTheme.textTheme.headlineLarge,
             ),
             subtitle: Text(
               'Manejo de notificaciones',
-              style: Theme.of(context).textTheme.bodySmall,
+              style: AppTheme.ligthTheme.textTheme.bodySmall,
             ),
-            leading: const Icon(Icons.notification_important),
-            trailing: const Icon(Icons.arrow_right_alt_outlined),
+            leading: const Icon(
+              Icons.notification_important,
+              color: AppTheme.primaryColor,
+            ),
+            trailing: const Icon(
+              Icons.arrow_right_alt_outlined,
+              color: AppTheme.primaryColor,
+            ),
           ),
         ],
       ),
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -3,11 +3,11 @@ import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   //Constante del valor primario
-  static const primaryColor = Colors.black12;
+  static const primaryColor = Colors.black;
+  //Constante del color secundario
+  static const secondaryColor = Color.fromARGB(255, 172, 21, 202);
   //Constante del color de fondo
   static const backColor = Color.fromARGB(255, 226, 226, 226);
-  //Constante del color secundario
-  static const secondaryColor = Color.fromARGB(255, 124, 98, 129);
 
   //Definición del tema de color y estilo de la app
   static final ThemeData ligthTheme = ThemeData.light().copyWith(
@@ -16,16 +16,19 @@ class AppTheme {
     textTheme: TextTheme(
       //Títulos de primer nivel
       headlineLarge: GoogleFonts.acme(
-        fontSize: 20.0,
-        fontWeight: FontWeight.bold,
-        color: primaryColor,
-      ),
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          color: primaryColor,
+          decoration: TextDecoration.underline,
+          decorationColor: primaryColor,
+          decorationStyle: TextDecorationStyle.double,
+          decorationThickness: 2.5),
       //Fuente para textos muy pequeños
       bodySmall: GoogleFonts.montserrat(
-        fontSize: 14.0,
-        fontWeight: FontWeight.w400,
-        color: secondaryColor,
-      ),
+          fontSize: 16.0,
+          fontWeight: FontWeight.w400,
+          color: secondaryColor,
+          fontStyle: FontStyle.italic),
     ),
   );
 }
