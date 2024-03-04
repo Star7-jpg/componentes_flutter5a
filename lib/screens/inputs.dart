cf25614ccb1a1ad23374cc784fcd7faf0ef65866
Star7-jpg <marcosconde612@gmail.com>
2024-03-04 13:01:43 -0600

    12 BottonNavigationBar and Slider

--- lib/screens/inputs.dart
+++ lib/screens/inputs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practica3/screens/data_screens.dart';
 import 'package:practica3/theme/app_theme.dart';
 
 class Inputs extends StatefulWidget {
@@ -9,31 +10,92 @@ class Inputs extends StatefulWidget {
 }
 
 class _InputsState extends State<Inputs> {
+  double sliderValue = 0.0;
+  bool valueSwitch = false;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          'Ventana de entradas',
-          style: AppTheme.ligthTheme.textTheme.headlineLarge,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Entradas',
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            ElevatedButton(
+            entradaTexto(),
+            entradaSwitch(),
+            entradasSlider(),
+            const ElevatedButton(
+              //style: AppTheme.ligthTheme.elevatedButtonTheme,
+              //Tarea realizar el código para pasar al DataSreen
               onPressed: null,
               child: Text(
-                'Regresar',
-                style: AppTheme.ligthTheme.textTheme.headlineLarge,
+                'DataSreen',
               ),
-            ),
-            const ElevatedButton(
-              onPressed: null,
-              child: Text('DataSreen'),
             ),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Inicio'),
+          BottomNavigationBarItem(icon: Icon(Icons.next_plan), label: 'Datos'),
+        ],
+      ),
+    );
+  }
+
+  TextField entradaTexto() {
+    return TextField(
+      style: AppTheme.ligthTheme.textTheme.headlineMedium,
+      decoration: InputDecoration(
+        border: const UnderlineInputBorder(),
+        labelText: 'Escribe tu nombre',
+        labelStyle: AppTheme.ligthTheme.textTheme.headlineLarge,
+      ),
+    );
+  }
+
+  Row entradaSwitch() {
+    return Row(
+      children: [
+        Text(
+          '¿Te gusta Flutter?',
+          style: AppTheme.ligthTheme.textTheme.headlineLarge,
+        ),
+        Switch(
+            value: valueSwitch,
+            onChanged: (value) {
+              setState(() {
+                valueSwitch = value;
+              });
+            })
+      ],
+    );
+  }
+
+  Column entradasSlider() {
+    return Column(
+      children: [
+        Text(
+          '¿Qué tanto te gusta flutter',
+          style: AppTheme.ligthTheme.textTheme.headlineLarge,
+        ),
+        Slider(
+            divisions: 10,
+            min: 0.0,
+            max: 10.0,
+            value: sliderValue,
+            label: '${sliderValue.round()}',
+            onChanged: (value) {
+              setState(() {
+                sliderValue = value;
+                // print('Valor del slider: $sliderValue');
+              });
+            })
       ],
     );
   }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -8,21 +8,57 @@ class AppTheme {
   static const secondaryColor = Color.fromARGB(255, 172, 21, 202);
   //Constante del color de fondo
   static const backColor = Color.fromARGB(255, 226, 226, 226);
+  //constante de color de botones y otros widgets
+  static const widgetsColor = Color.fromARGB(255, 68, 231, 19);
+  //constante de color de botones y otros widgets deshabilitados
+  static const disabledWidgetColor = Color.fromARGB(255, 132, 134, 150);
 
   //Definición del tema de color y estilo de la app
   static final ThemeData ligthTheme = ThemeData.light().copyWith(
+    //colores del tema claro
     scaffoldBackgroundColor: backColor,
-    appBarTheme: const AppBarTheme(color: secondaryColor),
+    appBarTheme: AppBarTheme(
+      color: secondaryColor,
+      titleTextStyle: GoogleFonts.lato(
+        color: backColor,
+        fontSize: 28.5,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    iconTheme: const IconThemeData(
+      color: primaryColor,
+      size: 35.0,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(widgetsColor),
+        textStyle: MaterialStateProperty.all(
+          GoogleFonts.pacifico(
+            fontSize: 22.5,
+          ),
+        ),
+      ),
+    ),
+
+    //estilo de los textos
     textTheme: TextTheme(
       //Títulos de primer nivel
       headlineLarge: GoogleFonts.acme(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          color: primaryColor,
-          decoration: TextDecoration.underline,
-          decorationColor: primaryColor,
-          decorationStyle: TextDecorationStyle.double,
-          decorationThickness: 2.5),
+        fontSize: 20.0,
+        fontWeight: FontWeight.bold,
+        color: primaryColor,
+        decoration: TextDecoration.underline,
+        decorationColor: primaryColor,
+        decorationStyle: TextDecorationStyle.double,
+        decorationThickness: 2.5,
+      ),
+
+      headlineMedium: GoogleFonts.akayaKanadaka(
+        fontSize: 25.0,
+        fontWeight: FontWeight.w700,
+        color: secondaryColor,
+      ),
+
       //Fuente para textos muy pequeños
       bodySmall: GoogleFonts.montserrat(
           fontSize: 16.0,
